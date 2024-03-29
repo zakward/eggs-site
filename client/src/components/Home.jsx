5dd4bc0d5a44924c3f7aa13f4d8c93b3f9de3cd7
Zak Ward <zakward85@gmail.com>
2024-03-29 16:34:40 -0500

    deployment push

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 import { useNavigate } from "react-router-dom"
 
 function Home() {
-    const [isMenuOpen, setIsMenuOpen] = useState(true)
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
     const navigate = useNavigate()
 
